b42cb35d13974dd6c5988a57e1a97cdfb9a4123b
Marta Stefaniak <vocado7@gmail.com>
2023-05-11 17:14:20 +0200

    fixes task context

--- src/helpers/TaskContext.tsx
+++ src/helpers/TaskContext.tsx
@@ -1,10 +1,5 @@
 import React, { createContext, Dispatch, ReactElement, useState } from 'react';
-import { Task } from '../App.tsx';
-
-// interface ContextTypes {
-//     tasks: Task[];
-//     setTasks: Dispatch<TaskAction | TaskGetFromApiAction>;
-// }
+import { Task } from './BasicTypes.ts';
 
 interface ContextTypes {
     tasks: Task[];
@@ -15,63 +10,6 @@ export const TasksContext = createContext<ContextTypes>({
     tasks: [],
     setTasks: () => null,
 });
-
-// interface TaskAction {
-//     type: 'finish' | 'delete' | 'submit';
-//     payload: Task;
-// }
-//
-// export interface TaskGetFromApiAction {
-//     type: 'getFromApi';
-//     tasksFromApi: Task[];
-//     operationsFromApi: Operation[];
-// }
-//
-// type InitialState = {
-//     tasks: Task[];
-// };
-//
-// const initialState: InitialState = {
-//     tasks: [],
-// };
-//
-// function tasksReducer(
-//     state: InitialState,
-//     action: TaskAction | TaskGetFromApiAction
-// ) {
-//     const { tasks } = state;
-//
-//     switch (action.type) {
-//         case 'finish': {
-//             return { ...state, tasks: [...tasks] };
-//         }
-//         case 'delete': {
-//             return {
-//                 ...state,
-//                 tasks: state.tasks.filter(
-//                     task => task.id !== action.payload.id
-//                 ),
-//             };
-//         }
-//         case 'getFromApi': {
-//             return {
-//                 ...state,
-//                 tasks: action.tasksFromApi.map(task => ({
-//                     ...task,
-//                     operations: action.operationsFromApi.filter(
-//                         operation => operation.taskId === task.id
-//                     ),
-//                 })),
-//             };
-//         }
-//         case 'submit': {
-//             return {
-//                 ...state,
-//                 tasks: [...tasks, { ...action.payload, operations: [] }],
-//             };
-//         }
-//     }
-// }
 
 type TasksProviderPropsType = {
     children: ReactElement;
